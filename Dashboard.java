7ebc6b47bed3da5149d5ec4c7d0ce742b3f92f5d
James Wolfe <wolfe.james.j96@gmail.com>
2022-12-02 15:49:14 -0500

    Updated 12/2 V.5 Changes: -Added arrays for bar chart -Changed buttons -Added columns for large sized page

--- Dashboard.java
+++ Dashboard.java
@@ -2,3 +2,24 @@ LocalData now = LocalData.now();
 int year = now.getYear();
 
 model.put("year", year);
+
+JSONArray revenueArray = new JSONArray();
+revenueArray.add(12);
+revenueArray.add(22);
+revenueArray.add(32);
+revenueArray.add(42);
+model.put("revenueArray", revenueArray);
+
+JSONArray networthArray = new JSONArray();
+networthArray.add(12);
+networthArray.add(22);
+networthArray.add(32);
+networthArray.add(42);
+model.put("networthArray", networthArray);
+
+JSONArray freecashflowArray = new JSONArray();
+freecashflowArray.add(12);
+freecashflowArray.add(22);
+freecashflowArray.add(32);
+freecashflowArray.add(42);
+model.put("freecashflowArray", freecashflowArray);
